76e2da2685412f23b1e39a66e018c68c4c3d42cf
saraswata-code <saraswattripathi.codearts@gmail.com>
2022-07-15 13:49:36 +0530

    edit profile module with latest update

---
+++ department.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+
+    <head>    
+        <!-- Header CSS files -->
+        <?php include 'header_css.php'; ?>
+        <?php
+            if($_SESSION['emp_id'] == '')
+            {
+              echo "<script>location.href='".$baseURL."login.php';</script>";
+            }
+        ?>
+        <title>Profile - CERM :: Codearts Employee Relationship Management</title>
+    </head>
+
+    <body>
+        <header class="custom-header">
+            <!-- Dashboard Top Info Panel -->
+            <?php include 'info_panel.php'; ?>
+        </header>
+        <main class="custom-dahboard-main">
+            <div class="custom-page-wrap-dp">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <!-- Dashboard Left Sidebar -->
+                            <?php include 'dashboard.php'; ?>
+                        </div>
+                        <div class="col-lg-9">
+                            <section class="inner-head-brd">
+                                <h2>Profile</h2>
+                                <ul>
+                                    <li><a href="<?php echo $baseURL; ?>">Home</a></li>
+                                    <li>Profile</li>
+                                </ul>
+                            </section>
